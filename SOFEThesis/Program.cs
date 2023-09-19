18bf517baa5a9902a355035a3ee815eab67755cd
a.ahmadi <a.ahmadi@papgroup>
2023-09-19 19:46:57 +0330

    add connection string and add context in program

--- SOFEThesis/Program.cs
+++ SOFEThesis/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SOFEThesis.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<SoleThesisContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("ConnStr")
+));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
